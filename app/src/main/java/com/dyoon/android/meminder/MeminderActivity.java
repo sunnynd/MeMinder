4f6a5aaec26e4c5c7ff877c8c2a306793a7bbeb7
sunny <dyoon0617@gmail.com>
2018-06-19 23:51:23 +0900

    Make the MeMinder Project

---
+++ app/src/main/java/com/dyoon/android/meminder/MeminderActivity.java
@@ -0,0 +1,13 @@
+package com.dyoon.android.meminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meminder);
+    }
+}
