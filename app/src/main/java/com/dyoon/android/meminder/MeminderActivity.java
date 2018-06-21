26fffa4ffc896df9c4c260f094c2b5b55835157f
sunny <dyoon0617@gmail.com>
2018-06-22 00:42:35 +0900

    Update the MemenderFragment

--- app/src/main/java/com/dyoon/android/meminder/MeminderActivity.java
+++ app/src/main/java/com/dyoon/android/meminder/MeminderActivity.java
@@ -1,5 +1,6 @@
 package com.dyoon.android.meminder;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MeminderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meminder);
+
+        if(savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            MeminderFragment meminderFragment = new MeminderFragment();
+            transaction.replace(R.id.frame_layout_meminder_fragment, meminderFragment);
+            transaction.commit();
+        }
     }
 }
